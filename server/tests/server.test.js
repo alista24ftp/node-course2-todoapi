2d706b51a8d1351e0cfadcc3a4e7b7239d80637a
alista24ftp <johnlittlefish@hotmail.com>
2016-12-19 18:23:28 -0800

    add tests for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -105,3 +105,41 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var todoId = dummyTodos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${todoId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(todoId);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.findById(todoId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((err) => {
+          done(err);
+        });
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var todoId = new ObjectID();
+    request(app)
+      .delete(`/todos/${todoId.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if objectID invalid', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
